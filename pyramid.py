8f292174ee9fcca43705b54587aa139737c751f9
Tirth Parmar <163869222+Tirthp1507@users.noreply.github.com>
2024-05-24 19:13:50 +0530

    Add files via upload

---
+++ pyramid.py
@@ -0,0 +1,32 @@
+def print_space(space):
+	if space > 0:
+		print("  ", end="")
+		print_space(space - 1)
+
+def print_star(star):
+	if star > 0:
+		print("* ", end="")
+		print_star(star - 1)
+
+def print_pyramid(n, current_row=1):
+	if current_row > n:
+		return
+
+	spaces = n - current_row
+	stars = 2 * current_row - 1
+
+	
+	print_space(spaces)
+
+	
+	print_star(stars)
+
+	
+	print()
+
+	print_pyramid(n, current_row + 1)
+
+n = 5
+
+
+print_pyramid(n)
